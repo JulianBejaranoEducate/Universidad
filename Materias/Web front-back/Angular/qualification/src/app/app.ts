7b75403a8abb7101e8eef690f9ace6fd959fe59e
Julian Bejarano <jbejarano@uniempresarial.edu.co>
2025-09-07 10:55:12 -0500

    Directive *ngFor

--- Materias/Web front-back/Angular/qualification/src/app/app.ts
+++ Materias/Web front-back/Angular/qualification/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, Directive, signal } from '@angular/core';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 import { DirectiveNgIf } from './components/directive-ng-if/directive-ng-if';
 import { DirectiveNgSw } from './components/directive-ng-sw/directive-ng-sw';
+import { DirectiveNgFor } from './components/directive-ng-for/directive-ng-for';
 
 @Component({
   selector: 'app-root',
   imports: [
     DirectiveNgIf,
-    DirectiveNgSw
+    DirectiveNgSw,
+    DirectiveNgFor,
+    Header,
+    Footer
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
